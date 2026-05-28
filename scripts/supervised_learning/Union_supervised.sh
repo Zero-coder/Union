c0576faa867e791e88396d25bb8ca6470f1ae351
JMW_1999 <54145971+Zero-coder@users.noreply.github.com>
2026-05-28 23:18:50 +0800

    Add supervised reproduction script

---
+++ scripts/supervised_learning/Union_supervised.sh
@@ -0,0 +1,27 @@
+model_name=Union
+exp_name=Union_supervised_x64
+wandb_mode=disabled
+project_name=supervised_learning
+
+random_port=$((RANDOM % 9000 + 1000))
+
+torchrun --nnodes 1 --nproc-per-node=1 --master_port $random_port run.py \
+  --is_training 1 \
+  --model_id $exp_name \
+  --model $model_name \
+  --lradj supervised \
+  --prompt_num 10 \
+  --patch_len 16 \
+  --stride 16 \
+  --e_layers 3 \
+  --d_model 64 \
+  --des 'Exp' \
+  --learning_rate 1e-4 \
+  --weight_decay 5e-6 \
+  --train_epochs 5 \
+  --batch_size 32 \
+  --acc_it 32 \
+  --debug $wandb_mode \
+  --project_name $project_name \
+  --clip_grad 100 \
+  --task_data_config_path data_provider/multi_task.yaml
